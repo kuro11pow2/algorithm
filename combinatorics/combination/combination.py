4aac3f42707d757ae4fe5fe593f8a1f545e44894
kuro11pow2 <kuro11pow2@gmail.com>
2021-10-17 02:19:47 +0900

    Add combination.py

---
+++ combinatorics/combination/combination.py
@@ -0,0 +1,32 @@
+import sys, os.path as op
+sys.path.append((pDir:=lambda x, n, i=0: pDir(op.abspath(op.dirname(x)), n, i+1) if i < n else x)(__file__, 3))
+from checkTime import checkTime
+
+from itertools import combinations
+
+@checkTime
+def myCombinations(li, n):
+    nLi = len(li)
+    arr = []
+    ret = []
+    def recurse(idx):
+        if len(arr) == n:
+            ret.append(tuple(map(lambda x: li[x], arr)))
+            return
+        for i in range(idx, nLi):
+            arr.append(i)
+            recurse(i + 1)
+            arr.pop()
+    recurse(0)
+    return ret
+
+if __name__ == "__main__":
+    items = [chr(ord('0') + i) for i in range(5)]
+    com = combinations(items, 3)
+    myCom = myCombinations(items, 3)
+    same = True
+    for a, b in zip(com, myCom):
+        if a != b:
+            same = False
+        print(a, b)
+    print(f'{same=}')
