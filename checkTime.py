8b8e91e29e1437ce88ae4aec01656e4a87109d8a
kuro11pow2 <kuro11pow2@gmail.com>
2021-10-17 02:19:31 +0900

    Add checkTime.py to measure the execution time

---
+++ checkTime.py
@@ -0,0 +1,11 @@
+import timeit
+
+def checkTime(f):
+    def wrapper(*args, **kw):
+        s = timeit.default_timer()
+        output = f(*args, **kw)
+        e = timeit.default_timer()
+        us = 1e-6
+        print(f'{f.__name__} execution time: {(e-s)/us:.4}us')
+        return output
+    return wrapper
